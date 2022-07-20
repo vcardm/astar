24b5659888ae67155da2b1dbce802054a4d23172
Valentina Cardenas <valentinacardm@gmail.com>
2022-07-20 14:51:14 -0400

    thread.sleep

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int row, col = 0;
 
         //b. getting users input 
@@ -18,6 +18,7 @@ public class Main {
                 System.out.println("Enter the starting node, specifying the column (y) Ensuring it is greater than 0: ");
                 col = in.nextInt();
             } while (!(col > 0));
+        
         // System.out.println("Enter the starting node, specifying the row and column (x y): ");
         // row = in.nextInt(); //save input in respected x,y -> row/columns
         // col = in.nextInt();
@@ -26,7 +27,10 @@ public class Main {
         //     System.out.println("NO path");
         // }
         Node firstNode = new Node(row, col);//save users input in firstNode as the starting node
+        System.out.println("Starting node: " + firstNode); //printout starting node
 
+        System.out.println("-----------------------------------------");
+        Thread.sleep(500);
         ////user input for goal node
         //do-while loop to ensure it is a poristive number for it be pathable  b i.
         System.out.println("-Goal Node-");
@@ -49,6 +53,11 @@ public class Main {
         //     System.out.println("NO path");
         // }
         Node goalNode = new Node(row, col); //save users input in goalNode as the final/goal node
+        System.out.println("Final node: " + goalNode);
+
+        System.out.println("-----------------------------------------");
+        Thread.sleep(500);
+
 
         int rows = 15;
         int cols = 15;
@@ -70,6 +79,8 @@ public class Main {
             
             row = 0;
             col = 0;
+
+            Thread.sleep(500);
 
             //b. i. displaying environemnt
         System.out.println("Envrionment:");
@@ -96,7 +107,7 @@ public class Main {
         System.out.println("From Start Node to Goal Node:");
         for (Node node : optimalPath) {
             //outputs path
-            System.out.print("["+node.getRow()+","+node.getCol()+"] ");
+            System.out.print("["+node.getRow()+","+node.getCol()+"] ->  ");
         }
     }
 }
